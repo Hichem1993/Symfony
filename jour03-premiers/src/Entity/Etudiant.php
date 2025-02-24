15674f4fe4b9f85319d1384e51946bb609d00944
Hichem-BEN-AYED <benayedhicham@gmail.com>
2025-02-24 12:54:26 +0100

    Doctrine

---
+++ jour03-premiers/src/Entity/Etudiant.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM ;  // Le coeur de Doctrine  ==> Mapper
+
+
+#[ORM\Entity()]
+class Etudiant{
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue()]
+    #[ORM\Column()]
+    private ?int $id = null;
+
+    #[ORM\Column(length:255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length:255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(options:["default" => 10 , "unsigned" => true], type:"smallint")]   // unsigned = chiffre positif
+    private ?int $age = null;
+
+    #[ORM\Column()]
+    private ?\DateTime $dt_naissance = null;
+
+    #[ORM\Column()]
+    private ?bool $is_admin = null;
+
+    #[ORM\Column(length:20)]
+    private ?string $telephone = null;
+
+}
