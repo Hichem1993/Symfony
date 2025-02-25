1d16bdaaf9e86ac0be0b0d505bac861a8ef2da37
Hichem-BEN-AYED <benayedhicham@gmail.com>
2025-02-25 14:11:09 +0100

    Accueil

--- jour07-tp/src/Controller/FrontController.php
+++ jour07-tp/src/Controller/FrontController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticlesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
 class FrontController extends AbstractController{
 
     #[Route("/" , name:"page_accueil")]
-    public function accueil(){
-        return $this->render("front/accueil.html.twig");
+    public function accueil(ArticlesRepository $articteRepo){
+        $articles = $articteRepo->findAll(); // SELECT * FROM Articles
+        return $this->render("front/accueil.html.twig" , ["articles" => $articles]);
     }
 }
